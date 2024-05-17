736a888ad493e5a7f990d871963aa7e81dd05a2a
ARJUN B <83937118+arjun-mighty@users.noreply.github.com>
2024-05-17 11:03:54 +0530

    Add files via upload
    this is temp test file

---
+++ Ac.java
@@ -0,0 +1,42 @@
+public class Ac
+{
+    public static void main(String args[])
+    {
+       /* int x = 7;
+       // int result =++num1;
+
+       // int result = num1++;
+       int y =5;
+       int a =5;
+       int b =8;
+       boolean result =a>b;
+       //<y || a<b;
+        System.out.println(!result);
+        //System.out.println("Miya");
+        
+       int i=0;
+       while(i<10)
+       {
+        System.out.println(i);
+        i++;
+        if(i==4)
+        {
+           break; //continue;
+        }
+        */
+      
+        int num =2;
+        switch(num+2)
+        {
+            case 1:
+                System.out.println("case 1: Value is :"+num);
+            case 2:
+                System.out.println("case 2: Value is :"+num);    
+            case 3:
+                System.out.println("case 3: Value is :"+num);    
+                default :
+                System.out.println("Default: Value is :"+num);
+        }
+    }
+}
+       
